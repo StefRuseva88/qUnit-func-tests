3c6ef1e895ab4b784c1c0fc3bf7725d6e7d73343
Stefaniya Ruseva <118391541+StefRuseva88@users.noreply.github.com>
2024-07-16 17:34:22 +0300

    Update isEven_tests.js

--- isEven_tests.js
+++ isEven_tests.js
@@ -1,14 +1,16 @@
 const { isEven } = require("./test_functions.js")
 
 QUnit.module("isEven function testing", () => {
-    QUnit.test("Even number", function(assert) {
+    QUnit.test("Even numbers", function(assert) {
         assert.ok(isEven(2), "Identifying even number: 2");
-        assert.notOk(isEven(3), "Identifying odd number: 3");
-        assert.ok(isEven(0), "Identifying even number: 0");
+        assert.ok(isEven(10), "Identifying even number: 10");
+        assert.ok(isEven(100), "Identifying even number: 100");
     });
 
-    QUnit.test("Odd number", function(assert) {
+    QUnit.test("Odd numbers", function(assert) {
         assert.notOk(isEven(3), "Identifying odd number: 3");
+        assert.notOk(isEven(3), "Identifying odd number: 9");
+        assert.notOk(isEven(3), "Identifying odd number: 21");
     });
 
     QUnit.test("Zero as parameter", function(assert) {
