fa404b8dcb9c0df93e60bd57dab4050775395be8
Stefaniya Ruseva <118391541+StefRuseva88@users.noreply.github.com>
2024-07-16 17:49:01 +0300

    Update factorial_tests.js

--- factorial_tests.js
+++ factorial_tests.js
@@ -5,7 +5,7 @@ QUnit.module("factorial function testing", () => {
         assert.equal(factorial(5), 120, "Factorial of 5 is 120");
     });
 
-    QUnit.test("Zero as parameter", function(assert) {
+    QUnit.test("Zero as input", function(assert) {
         assert.equal(factorial(0), 1, "Factorial of 0 is 1");
     });
 
