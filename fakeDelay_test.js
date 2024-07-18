5b89fc61243cd587928f63d443b50240dba255e3
Stefaniya Ruseva <118391541+StefRuseva88@users.noreply.github.com>
2024-07-18 14:59:19 +0300

    Update fakeDelay_test.js

--- fakeDelay_test.js
+++ fakeDelay_test.js
@@ -1,6 +1,6 @@
 const {fake_delay} = require("./async_test_functions.js");
 
-QUnit.test("Delay Function Tests", async function(assert) {
+QUnit.test("Test with 1 sec delay", async function(assert) {
     const start = Date.now();
     await fake_delay(1000);
     const end = Date.now();
